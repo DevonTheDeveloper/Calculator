a8e9eec0b567b8985c10712d37026ca672cc7dc1
Devon <122610607+DevonTheDeveloper@users.noreply.github.com>
2023-02-10 22:07:16 +0100

    Add eventListeners & operate function

--- main.js
+++ main.js
@@ -1,3 +1,15 @@
+const digitButtons = document.querySelectorAll(".digits").forEach((item) => {
+  item.addEventListener("click", (event) => {
+    let num1 = event.target.value;
+  });
+});
+
+const operators = document.querySelectorAll('.operators').forEach((item) => {
+  item.addEventListener('click', (event) => {
+    let operator = event.target.value;
+  })
+})
+
 function add(a, b) {
   return a + b;
 }
@@ -7,9 +19,21 @@ function subtract(a, b) {
 }
 
 function multiply(a, b) {
-    return a * b;
+  return a * b;
 }
 
 function divide(a, b) {
-    return a / b;
+  return a / b;
+}
+
+function operate(operator, num1, num2) {
+  if (operator === "+") {
+    return add(num1, num2);
+  } else if (operator === "-") {
+    return subtract(num1, num2);
+  } else if (operator === "*") {
+    return multiply(num1, num2);
+  } else if (operator === "/") {
+    return divide(num1, num2);
+  }
 }
